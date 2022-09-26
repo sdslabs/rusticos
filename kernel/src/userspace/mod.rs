e2487b510dbb97d79bc385ad314da459f321b95d
Ashutosh Srivastava <cyberboy9971@gmail.com>
2022-09-26 20:49:58 +0530

    Add random userspace function. Add comments

--- kernel/src/userspace/mod.rs
+++ kernel/src/userspace/mod.rs
@@ -9,6 +9,8 @@ pub mod userspace;
 //     2) Setting the cs and ds registers to proper indexes in the GDT to indicate that we are currently in Ring3 or usermode
 //     3) Setting the registers for sysretq and iretq operations
 
+//Obviously you can remove the jmp statement from the program from infinitely looping
+
 pub unsafe fn userspace_prog_1() {
     asm!("\
         start:
@@ -24,3 +26,38 @@ pub unsafe fn userspace_prog_1() {
 
 //The syscall should automatically invoke handlers::process_syscall with the following registers set as rax - syscall address, and rdi rsi rdx r10 are just 4 registers for 4 arguments passed to the syscall. 
 //You can increase this by increasing the number of arguments the syscall can handle and setting the respective registers with the appropriate values in the userspace program. :)
+
+
+//Random user space program which does something (has miniloops) and calls syscalls
+#[naked]
+pub unsafe fn userspace_prog_1() {
+    asm!("\
+        mov rbx, 0xf0000000
+        prog1start:
+        push 0x595ca11a // keep the syscall number in the stack
+        mov rbp, 0x0 // distinct values for each register
+        mov rax, 0x1
+        mov rcx, 0x3
+        mov rdx, 0x4
+        mov rdi, 0x6
+        mov r8, 0x7
+        mov r9, 0x8
+        mov r10, 0x9
+        mov r11, 0x10
+        mov r12, 0x11
+        mov r13, 0x12
+        mov r14, 0x13
+        mov r15, 0x14
+        xor rax, rax
+        prog1loop:
+        inc rax
+        cmp rax, 0x4000000
+        jnz prog1loop // loop for some milliseconds
+        pop rax // pop syscall number from the stack
+        inc rbx // increase loop counter
+        mov rdi, rsp // first syscall arg is rsp
+        mov rsi, rbx // second syscall arg is the loop counter
+        syscall // perform the syscall!
+        jmp prog1start // do it all over
+    ");
+}
