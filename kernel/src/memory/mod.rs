dd35c52faf755edaff637f3ae1f9ac6534d40f36
Ashutosh Srivastava <cyberboy9971@gmail.com>
2022-09-27 23:30:05 +0530

    Remove obsolete mapper

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -22,49 +22,6 @@ use x86_64::{
     PhysAddr,
 };
 
-/* This code must not be used because we will use our own FrameAllocator
-/// Creates an example mapping for the given page to frame `0xb8000`.
-pub fn create_example_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
-
-    let map_to_result = unsafe {
-        // FIXME: this is not safe, we do it only for testing
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush();
-}
-
-// WIP hashkat
-pub fn create_user_process1_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    //Change frame address to a random address here... Not sure but I think it must be handled by the frame allocator? CC: Nautilus -->
-    // Taking 0x400000 as virt address
-    // Taking 0x800000 as physical address
-    // Copy Kernel's PT entries into a new one ---> Help needed here CC Ayan
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE | Flags::USER_ACCESSIBLE;
-
-    let map_to_result = unsafe {
-        // FIXME: this is not safe, we do it only for testing
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush();
-}
-
-*/
-
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
 pub struct BootInfoFrameAllocator {
